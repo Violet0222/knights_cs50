4fed31abe6a9bf1bc73bb4192236fcc5f9e53f68
vitakolosova <kolosova.dev@gmail.com>
2025-05-17 22:54:47 +0200

    add puzzle 0

--- puzzle.py
+++ puzzle.py
@@ -12,14 +12,22 @@ CKnave = Symbol("C is a Knave")
 # Puzzle 0
 # A says "I am both a knight and a knave."
 knowledge0 = And(
-    # TODO
+    Or(AKnight, AKnave),
+    Not(And(AKnight, AKnave)),
+    Or(BKnight, BKnave),
+    Not(And(BKnight, BKnave)),
+    Or(CKnight, CKnave),
+    Not(And(CKnight, CKnave)),
+    
+    Implication(AKnight, And(AKnight,AKnave)),
+    Implication(AKnave, Not(And(BKnight,BKnave))),
 )
 
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
 knowledge1 = And(
-    # TODO
+   
 )
 
 # Puzzle 2
